bea7274d92a213197e69208a16b01a95b7285b5d
Mohamed <mhamoudi@avans.nl>
2018-12-21 10:48:13 +0100

    Added string container

---
+++ cpp1_eindopdracht/cpp1_eindopdracht/MyString.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+
+class MyString
+{
+public:
+	MyString();
+	MyString(const char *string);
+	MyString(const char *string, int length);
+	~MyString();
+
+	MyString& operator=(const MyString& other);
+	MyString& operator=(MyString&& other);
+	MyString(const MyString& other);
+	MyString(MyString&& other);
+
+	int Parse();
+
+	char GetLetter(int index) const;
+	int GetLength() const;
+private:
+	char *string = '\0';
+	int length;
+	void SetString(const char* string, int length);
+};
+
